9f03cf342f58d53089d905ea0d39e1a6f36c49cf
Mushbt <mustafa_habet@hotmail.com>
2023-11-03 16:28:08 +0000

    commit

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile_id')
+    profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
