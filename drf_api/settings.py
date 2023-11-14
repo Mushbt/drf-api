7b4c53b7c571913c1f1eac168fb7c133d7f312a9
Mushbt <mustafa_habet@hotmail.com>
2023-11-14 14:22:25 +0000

    commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV'
 
 ALLOWED_HOSTS = ['8000-mushbt-drfapi-iqve0pervfu.ws-eu105.gitpod.io', 'localhost', os.environ.get('ALLOWED_HOST')]
 
